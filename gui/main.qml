0e4fa97b73ed3b8fb4d3a46e04504136b041e544
Spencer Brown <spencerb@warpmail.net>
2017-05-18 14:14:41 -0400

    Fixed final issues with build system change.

--- gui/main.qml
+++ gui/main.qml
@@ -6,7 +6,7 @@ ApplicationWindow {
     visible: true
     width: 640
     height: 480
-    title: qsTr("StaunchNotes")
+    title: qsTr("Triad")
     id: root
 
     RowLayout {
--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    engine.load(QUrl(QLatin1String("qrc:/main.qml")));
+    engine.load(QUrl(QLatin1String("qrc:/gui/main.qml")));
 
     return app.exec();
 }
